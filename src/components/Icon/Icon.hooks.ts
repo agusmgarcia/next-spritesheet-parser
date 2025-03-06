52871676f043ca104086da12e081fea35f5dccd3
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-06 00:16:33 -0300

    refactor(Icon): move some properties into the hook

--- src/components/Icon/Icon.hooks.ts
+++ src/components/Icon/Icon.hooks.ts
@@ -4,6 +4,9 @@ export default function useIcon(props: IconProps) {
   return {
     ...props,
     fill: "currentColor",
+    height: 24,
+    viewBox: "0 0 24 24",
+    width: 24,
     xmlns: "http://www.w3.org/2000/svg",
   };
 }
