26f097fd4fcfbcd4ab41cfc11380493baf810edd
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-10 01:48:33 -0300

    fix(Input): set hover and focus styles for color variant

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -14,6 +14,7 @@ export default function Input(props: InputProps) {
           "size-full cursor-pointer appearance-none rounded-lg border border-black bg-white p-2 outline outline-1 outline-offset-0 outline-transparent",
           "transition-colors",
           "focus-within:-outline-offset-2 focus-within:outline-cranberry-600",
+          "enabled:hover:bg-cranberry-300 enabled:focus:bg-cranberry-300",
           "disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400",
           className,
         )}
