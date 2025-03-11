e49fc7e9f0f101acf2c419b7e831e4edb1af7c90
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-11 20:55:10 -0300

    feat(useDimensions): add utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as loadImage } from "./loadImage";
 export { type default as SetValue } from "./SetValue.types";
 export { default as useDevicePixelRatio } from "./useDevicePixelRatio";
+export { default as useDimensions } from "./useDimensions";
 export { default as useViewport } from "./useViewport";
