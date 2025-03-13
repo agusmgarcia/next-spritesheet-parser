b2b3e4630ee122536c7a2a2df1b5c0e459af5e09
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-13 10:34:53 -0300

    feat(Input): add placeholder, step and name properties

--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -1,6 +1,15 @@
 type InputProps = Pick<
   React.InputHTMLAttributes<HTMLInputElement>,
-  "aria-label" | "className" | "disabled" | "max" | "min" | "onChange" | "value"
+  | "aria-label"
+  | "className"
+  | "disabled"
+  | "max"
+  | "min"
+  | "name"
+  | "onChange"
+  | "placeholder"
+  | "step"
+  | "value"
 > & { type?: "color" | "number" | "text" };
 
 export default InputProps;
