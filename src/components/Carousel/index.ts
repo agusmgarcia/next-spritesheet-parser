3d95cce14dc2c0ca01ac7324319b6cc6dc73a270
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-14 03:36:21 -0300

    chore(Carousel): remove component

--- src/components/Carousel/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./Carousel";
-export { type default as CarouselProps } from "./Carousel.types";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { type AccordionProps, default as Accordion } from "./Accordion";
 export { type ButtonProps, default as Button } from "./Button";
-export { type CarouselProps, default as Carousel } from "./Carousel";
 export { default as Icon, type IconProps } from "./Icon";
 export { default as Input, type InputProps } from "./Input";
 export { default as Select, type SelectProps } from "./Select";
