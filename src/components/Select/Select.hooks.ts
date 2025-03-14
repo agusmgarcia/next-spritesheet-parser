be44bf8dd6cf0c1b7353ddaf662cd89a9cc45f58
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-13 21:30:54 -0300

    fix(Select): add arrow

--- src/components/Select/Select.hooks.ts
+++ src/components/Select/Select.hooks.ts
@@ -1,5 +1,21 @@
+import React, { useMemo } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+
+import { Icon } from "#src/components";
+
 import type SelectProps from "./Select.types";
 
 export default function useSelect(props: SelectProps) {
-  return { ...props };
+  const style = useMemo<React.CSSProperties>(
+    () => ({
+      backgroundImage: `url("data:image/svg+xml;utf8,${renderToStaticMarkup(
+        React.createElement(Icon, { variant: "arrowDown" }),
+      )
+        .replaceAll(`"`, `'`)
+        .replaceAll("#", "%23")}")`,
+    }),
+    [],
+  );
+
+  return { ...props, style };
 }
