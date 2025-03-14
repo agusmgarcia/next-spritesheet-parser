2711ca377333fd120a20c6a74e017110eb893900
Agus Garcia <agusss.garcia19@gmail.com>
2025-03-14 04:34:08 -0300

    fix(AppPage): style mobile version

--- src/pages/AppPage/AppPage.tsx
+++ src/pages/AppPage/AppPage.tsx
@@ -7,18 +7,27 @@ import type AppPageProps from "./AppPage.types";
 export default function AppPage(props: AppPageProps) {
   const { children, version, viewport } = useAppPage(props);
 
-  if (viewport === "Mobile")
-    return <Typography>This app is not intended to be for Mobile</Typography>;
-
   return (
     <main className="relative flex h-dvh w-screen border-[16px] border-dark">
-      {/* CHILDREN */}
-      <div className="h-full max-w-[calc(100%-360px)] flex-[1_0_67%] overflow-auto">
-        {children}
-      </div>
+      {viewport === "Mobile" && (
+        <div className="size-full bg-gray-800">
+          <Typography className="p-2 italic text-white">
+            This app is not intended to be for Mobile.
+          </Typography>
+        </div>
+      )}
 
-      {/* SIDEBAR */}
-      <SideBar.Container className="h-full max-w-[360px] flex-[1_0_33%] overflow-y-auto overflow-x-hidden bg-gray-800 p-4 pb-8" />
+      {viewport !== "Mobile" && (
+        <>
+          {/* CHILDREN */}
+          <div className="h-full max-w-[calc(100%-360px)] flex-[1_0_67%] overflow-auto">
+            {children}
+          </div>
+
+          {/* SIDEBAR */}
+          <SideBar.Container className="h-full max-w-[360px] flex-[1_0_33%] overflow-y-auto overflow-x-hidden bg-gray-800 p-4 pb-8" />
+        </>
+      )}
 
       {/* VERSION */}
       <div className="absolute bottom-2 right-2">
