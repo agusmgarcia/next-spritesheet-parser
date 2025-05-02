664ed18201c0f582efd06ad6aadfb1c0ad667f90
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-02 01:17:20 -0300

    feat(Icon): add error variant

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -6,6 +6,7 @@ type IconProps = Pick<React.SVGProps<SVGSVGElement>, "className"> & {
     | "arrowUp"
     | "backward"
     | "downloadFile"
+    | "error"
     | "forward"
     | "home"
     | "merge"
