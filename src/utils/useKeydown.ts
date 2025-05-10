676289f9fd59d0fc0dd7fef9922e61a02bf01a2e
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-10 14:58:40 -0300

    fix(useKeyDown): remove uneeded options

--- src/utils/useKeydown.ts
+++ src/utils/useKeydown.ts
@@ -4,32 +4,20 @@ import { useEffect } from "react";
 export default function useKeyDown(
   key: string,
   callback: Func,
-  options?: Partial<{
-    altKey: boolean;
-    ctrlKey: boolean;
-    metaKey: boolean;
-    shiftKey: boolean;
-  }>,
+  options?: Partial<{ altKey: boolean }>,
 ) {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.target instanceof HTMLInputElement) return;
       if (event.key !== key) return;
       if (!!options?.altKey !== event.altKey) return;
-      if (!!options?.ctrlKey !== event.ctrlKey) return;
-      if (!!options?.metaKey !== event.metaKey) return;
-      if (!!options?.shiftKey !== event.shiftKey) return;
+      if (event.ctrlKey) return;
+      if (event.metaKey) return;
+      if (event.shiftKey) return;
       callback();
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [
-    callback,
-    key,
-    options?.altKey,
-    options?.ctrlKey,
-    options?.metaKey,
-    options?.shiftKey,
-  ]);
+  }, [callback, key, options?.altKey]);
 }
