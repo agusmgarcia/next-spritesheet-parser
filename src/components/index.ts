f7097dbe3ab99abab2e40cf878ad31c01e4e73e2
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-10 01:14:11 -0300

    feat(Icon): add interrogation variant

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export { type ButtonProps, default as Button } from "./Button";
 export { default as Divisor, type DivisorProps } from "./Divisor";
 export { default as Icon, type IconProps } from "./Icon";
 export { default as Input, type InputProps } from "./Input";
+export { default as Modal, type ModalProps } from "./Modal";
 export { default as Select, type SelectProps } from "./Select";
 export { default as Typography, type TypographyProps } from "./Typography";
