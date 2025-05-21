3308128fff18fd51a2fb6bbdd97c5c84237601b1
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-21 02:05:24 -0300

    fix(HomePage): add instruction to reset zoom

--- src/pages/HomePage/HomePage.hooks.ts
+++ src/pages/HomePage/HomePage.hooks.ts
@@ -51,6 +51,10 @@ export default function useHomePage(props: HomePageProps) {
             description: "Zoom out",
             key: "ArrowDown",
           },
+          {
+            description: "Reset zoom",
+            key: "z",
+          },
         ],
         title: "Zoom",
       },
