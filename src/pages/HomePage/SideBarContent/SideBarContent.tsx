ea30b1e440a374a0d6a9b4bf1ed721da02079bcb
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-21 01:38:41 -0300

    chore(HomePage): add missing hook

--- src/pages/HomePage/SideBarContent/SideBarContent.tsx
+++ src/pages/HomePage/SideBarContent/SideBarContent.tsx
@@ -3,13 +3,16 @@ import { Accordion, Typography } from "#src/components";
 import AnimationsItem from "./AnimationsItem";
 import FilesItem from "./FilesItem";
 import SettingsItem from "./SettingsItem";
+import useSideBarContent from "./SideBarContent.hooks";
 import type SideBarContentProps from "./SideBarContent.types";
 import ZoomItem from "./ZoomItem";
 
 export default function SideBarContent(props: SideBarContentProps) {
+  const rest = useSideBarContent(props);
+
   return (
     <Accordion
-      {...props}
+      {...rest}
       heading={
         <Typography className="text-white" variant="h1">
           Home
