7187afa6ebf4c11221f142a8cc62c08a282a7a3a
Agus Garcia <agusss.garcia19@gmail.com>
2025-05-24 15:38:13 -0300

    fix(HomePage): remove name from the selector when empty

--- src/pages/HomePage/SideBarContent/AnimationsItem/AnimationsItem.hooks.ts
+++ src/pages/HomePage/SideBarContent/AnimationsItem/AnimationsItem.hooks.ts
@@ -164,12 +164,17 @@ function useAnimationSelector() {
 
   const animationSelectorOptions = useMemo<{ id: string; name: string }[]>(
     () => [
-      { id: "sheet", name: spriteSheet?.image.name || "Sprite sheet" },
+      {
+        id: "sheet",
+        name: !!spriteSheet?.image.url
+          ? spriteSheet.image.name
+          : "Sprite sheet",
+      },
       ...animations
         .map((a) => ({ id: a.id, name: a.name }))
         .sort((a1, a2) => sorts.byStringAsc(a1.name, a2.name)),
     ],
-    [animations, spriteSheet?.image.name],
+    [animations, spriteSheet?.image.name, spriteSheet?.image.url],
   );
 
   const animationSelectorOnChange = useCallback<
