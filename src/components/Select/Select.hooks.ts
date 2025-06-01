7f791afdd8333e4586ca3bde7402496a62b0f7eb
Agus Garcia <agusss.garcia19@gmail.com>
2025-06-01 11:45:55 -0300

    fix(Select): consume colors from tailwind

--- src/components/Select/Select.hooks.ts
+++ src/components/Select/Select.hooks.ts
@@ -1,16 +1,24 @@
 import React, { useMemo } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
+import tailwindConfig from "tailwind.config";
+import resolveConfig from "tailwindcss/resolveConfig";
 
 import { Icon } from "#src/components";
 
 import type SelectProps from "./Select.types";
+
+const config = resolveConfig(tailwindConfig);
 
 export default function useSelect(props: SelectProps) {
   const style = useMemo<React.CSSProperties>(
     () => ({
       backgroundImage: `url("data:image/svg+xml;utf8,${renderToStaticMarkup(
         React.createElement(Icon, {
-          style: { color: !props.disabled ? "black" : "rgb(156 163 175)" },
+          style: {
+            color: !props.disabled
+              ? config.theme.colors.black
+              : config.theme.colors.gray[400],
+          },
           variant: "arrowDown",
         }),
       )
