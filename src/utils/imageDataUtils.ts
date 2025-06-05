360452f7bd992a1a53c1c2d72cf575372c7d9fdd
Agus Garcia <agusss.garcia19@gmail.com>
2025-06-05 20:27:58 -0300

    fix: adjust background color calculation

--- src/utils/imageDataUtils.ts
+++ src/utils/imageDataUtils.ts
@@ -49,7 +49,7 @@ export function getBackground(imageData: ImageData): Tuple<number, 3> {
 
 export function getBackgroundColor(imageData: ImageData): string {
   const backgroundColor = getBackground(imageData);
-  return `#${backgroundColor[0].toString(16)}${backgroundColor[1].toString(16)}${backgroundColor[2].toString(16)}`;
+  return `#${backgroundColor[0].toString(16).padEnd(2, "0")}${backgroundColor[1].toString(16).padEnd(2, "0")}${backgroundColor[2].toString(16).padEnd(2, "0")}`;
 }
 
 export function generateNormalMap(
